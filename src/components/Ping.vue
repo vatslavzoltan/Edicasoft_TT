39924072040ffa5d380c9b87d7e4b769e88c48af
vatslav.zoltan@gmail.com <vatslav.zoltan@gmail.com>
2020-08-06 14:39:43 +0300

    Project set up :rocket:

---
+++ src/components/Ping.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ping',
+  data () {
+    return {
+      msg: 'Hello!'
+    }
+  },
+  methods: {
+    Users () {
+      this.$http
+        .get(this.$API_URL)
+        .then(response => {
+          console.log('getMessage -> response', response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.Users()
+  }
+}
+</script>
