dbe8b7a49f0e9c83c9856c9d5923b3dab0742531
vatslav.zoltan@gmail.com <vatslav.zoltan@gmail.com>
2020-08-06 18:08:11 +0300

    Basic form validation

--- src/components/AddUserForm.vue
+++ src/components/AddUserForm.vue
@@ -3,28 +3,30 @@
         <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" class="w-100" v-if="!isSuccess">
 
             <b-form-group id="form-name-group" label="Username:" label-for="form-name-input">
-                <b-form-input id="form-name-input" type="text" v-model="addUserForm.username" required placeholder="Enter name"/>
+                <b-form-input id="form-name-input" type="text" v-model="addUserForm.username"  placeholder="Enter name"/>
             </b-form-group>
+            <span class="error">{{errors.username}}</span>
 
             <b-form-group id="form-email-group" label="Email:" label-for="form-email-input">
-                <b-form-input id="form-email-input" type="text" v-model="addUserForm.email" required placeholder="Enter email"/>
+                <b-form-input id="form-email-input" type="text" v-model="addUserForm.email"  placeholder="Enter email"/>
             </b-form-group>
+            <span class="error">{{errors.email}}</span>
 
             <hr>
             <h5>Address:</h5>
             <b-form-group id="form-city-group" label="City:" label-for="form-city-input">
-                <b-form-input id="form-city-input" type="text" v-model="addUserForm.address.city" required placeholder="Enter city"/>
+                <b-form-input id="form-city-input" type="text" v-model="addUserForm.address.city"  placeholder="Enter city"/>
             </b-form-group>
 
             <b-form-group id="form-street-group" label="Street:" label-for="form-street-input">
-                <b-form-input id="form-street-input" type="text" v-model="addUserForm.address.street" required placeholder="Enter street"/>
+                <b-form-input id="form-street-input" type="text" v-model="addUserForm.address.street"  placeholder="Enter street"/>
             </b-form-group>
 
             <b-form-group id="form-block-group" label="Suite:" label-for="form-block-input">
-                <b-form-input id="form-block-input" type="text" v-model="addUserForm.address.suite" required placeholder="Enter suite"/>
+                <b-form-input id="form-block-input" type="text" v-model="addUserForm.address.suite"  placeholder="Enter suite"/>
             </b-form-group>
 
-            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="submit" variant="primary" >Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
 
         </b-form>
@@ -43,6 +45,10 @@ export default {
   data () {
     return {
       isSuccess: false,
+      errors: {
+        email: '',
+        username: ''
+      },
       successMessage: '',
       addUserForm: {
         username: '',
@@ -88,10 +94,42 @@ export default {
     },
     onSubmit () {
       const data = this.addUserForm
-      this.addUser(data)
+      if (this.checkForm()) {
+        this.addUser(data)
+      }
     },
     onReset () {
       this.resetForm()
+    },
+    isEmailCorrect (email) {
+      // validation using regex
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    checkForm () {
+      // if email not empty and valid
+      if (this.addUserForm.email && this.addUserForm.username && this.isEmailCorrect(this.addUserForm.email)) {
+        this.errors = []
+        return true
+      }
+      // if email empty
+      if (!this.addUserForm.email) {
+        this.errors = []
+        this.errors.email = 'The field cannot be empty.'
+        return false
+      }
+      // if name empty
+      if (!this.addUserForm.username) {
+        this.errors = []
+        this.errors.username = 'The field cannot be empty.'
+        return false
+      }
+      // if email not valid
+      if (!this.isEmailCorrect(this.addUserForm.email)) {
+        this.errors = []
+        this.errors.email = 'Please enter a valid email address'
+        return false
+      }
     }
   }
 
@@ -101,5 +139,9 @@ export default {
     .success_header{
         color: #28A745;
         text-align: center;
+    }
+    .error{
+      font-size: 12px;
+      color: #C82333;
     }
 </style>
--- src/components/EditUserForm.vue
+++ src/components/EditUserForm.vue
@@ -3,25 +3,27 @@
         <b-form @submit.prevent="onSubmit" class="w-100" v-if="!isSuccess">
 
             <b-form-group id="form-name-group" label="Username:" label-for="form-name-input">
-                <b-form-input id="form-name-input" type="text" v-model="user.username" required placeholder="Enter name"/>
+                <b-form-input id="form-name-input" type="text" v-model="user.username" placeholder="Enter name"/>
             </b-form-group>
+            <span class="error">{{errors.username}}</span>
 
             <b-form-group id="form-email-group" label="Email:" label-for="form-email-input">
-                <b-form-input id="form-email-input" type="text" v-model="user.email" required placeholder="Enter email"/>
+                <b-form-input id="form-email-input" type="text" v-model="user.email" placeholder="Enter email"/>
             </b-form-group>
+            <span class="error">{{errors.email}}</span>
 
             <hr>
             <h5>Address:</h5>
             <b-form-group id="form-city-group" label="City:" label-for="form-city-input">
-                <b-form-input id="form-city-input" type="text" v-model="user.address.city" required placeholder="Enter city"/>
+                <b-form-input id="form-city-input" type="text" v-model="user.address.city" placeholder="Enter city"/>
             </b-form-group>
 
             <b-form-group id="form-street-group" label="Street:" label-for="form-street-input">
-                <b-form-input id="form-street-input" type="text" v-model="user.address.street" required placeholder="Enter street"/>
+                <b-form-input id="form-street-input" type="text" v-model="user.address.street" placeholder="Enter street"/>
             </b-form-group>
 
             <b-form-group id="form-block-group" label="Suite:" label-for="form-block-input">
-                <b-form-input id="form-block-input" type="text" v-model="user.address.suite" required placeholder="Enter suite"/>
+                <b-form-input id="form-block-input" type="text" v-model="user.address.suite" placeholder="Enter suite"/>
             </b-form-group>
 
             <b-button type="submit" variant="primary">Submit</b-button>
@@ -45,7 +47,11 @@ export default {
   data () {
     return {
       isSuccess: false,
-      successMessage: ''
+      successMessage: '',
+      errors: {
+        email: '',
+        username: ''
+      },
     }
   },
   methods: {
@@ -71,9 +77,41 @@ export default {
       // Hide form
       this.$refs.editUserModal.hide()
     },
+    isEmailCorrect (email) {
+      // validation using regex
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    checkForm () {
+      // if email not empty and valid
+      if (this.user.email && this.user.username && this.isEmailCorrect(this.user.email)) {
+        this.errors = []
+        return true
+      }
+      // if email empty
+      if (!this.user.email) {
+        this.errors = []
+        this.errors.email = 'The field cannot be empty.'
+        return false
+      }
+      // if name empty
+      if (!this.user.username) {
+        this.errors = []
+        this.errors.username = 'The field cannot be empty.'
+        return false
+      }
+      // if email not valid
+      if (!this.isEmailCorrect(this.user.email)) {
+        this.errors = []
+        this.errors.email = 'Please enter a valid email address'
+        return false
+      }
+    },
     onSubmit () {
       const data = this.user
-      this.updateUser(data)
+      if (this.checkForm()) {
+        this.updateUser(data)
+      }
     }
   }
 
@@ -83,5 +121,9 @@ export default {
     .success_header{
         color: #28A745;
         text-align: center;
+    }
+    .error{
+      font-size: 12px;
+      color: #C82333;
     }
 </style>
